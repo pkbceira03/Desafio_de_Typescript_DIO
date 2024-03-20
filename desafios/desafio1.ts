354c3138a62ec212f767bfec25cd1a2845a9e543
pkbceira03 <cabeceira2003@gmail.com>
2024-03-20 19:27:04 -0300

    desafios 1, 2 e 3

---
+++ desafios/desafio1.ts
@@ -0,0 +1,13 @@
+// desafio (https://github.com/lira1705/mentoria-typescript/blob/main/src/desafios/desafio1.js)
+
+const employee = {
+    code: 10,
+    name: "Jonh"
+}
+
+// outra aulternativa
+
+interface empregado{
+    codew: number,
+    name: string
+}
---
+++ desafios/desafio2.ts
@@ -0,0 +1,25 @@
+// desafio (https://github.com/lira1705/mentoria-typescript/blob/main/src/desafios/desafio2.js)
+
+interface pessoa{
+    nome:string,
+    idade:number,
+    profissao:string
+}
+
+let pessoa1: pessoa = {
+    nome:'maria',
+    idade:67,
+    profissao:"engenheira"
+}
+
+let pessoa2: pessoa = {
+    nome:'pedro',
+    idade:34,
+    profissao:"engenheira"
+}
+
+let pessoa3: pessoa = {
+    nome:'bruna',
+    idade:23,
+    profissao:"engenheira"
+}
